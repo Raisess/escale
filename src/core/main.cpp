4b8e7a2a9289bb1744535de5d5d4a4a1784b2260
Raisess <raisechif@gmail.com>
2022-06-05 19:09:26 -0300

    core: update test

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -3,23 +3,17 @@
 
 int main() {
   Cache* cache = new Cache();
+
   cache->read_from_disk();
+  cache->set("A random key", "A random value");
 
-  /*cache->set("Key", "Value");
-  cache->set("Test", "Test");
-  cache->set("Test2", "Test2");
-  cache->set("Test2", "Test2");
-  cache->set("Danilo", "Danilo");
-  cache->set("Diff", "Key");*/
-
+  std::cout << "A random key: " << cache->get("A random key") << std::endl;
   std::cout << "Key: " << cache->get("Key") << std::endl;
   std::cout << "Key2: " << cache->get("Key2") << std::endl;
   std::cout << "Test: " << cache->get("Test") << std::endl;
   std::cout << "Test2: " << cache->get("Test2") << std::endl;
   std::cout << "Diff: " << cache->get("Diff") << std::endl;
   std::cout << "Danilo: " << cache->get("Danilo") << std::endl;
-
-  //cache->save_on_disk();
 
   return 0;
 }

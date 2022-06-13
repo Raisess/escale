ee135a50d2cb3735854e45312cfc48a349354883
Raisess <raisechif@gmail.com>
2022-06-13 09:01:55 -0300

    HashTable: improve remove item method

--- src/Common/HashTable.cpp
+++ src/Common/HashTable.cpp
@@ -25,15 +25,12 @@ void Common::HashTable::append(std::string key, std::string value, const long in
 
 void Common::HashTable::remove(std::string key) {
   unsigned int hash = this->hash(key);
-  Node<HashBlock>* ptr;
 
   this->table[hash]->for_each([&](Node<HashBlock>* node) -> void {
     if (node->data.key == key) {
-      ptr = node;
+      this->table[hash]->remove_node(node);
     }
   });
-
-  this->table[hash]->remove_node(ptr);
 }
 
 void Common::HashTable::for_each(std::function<void(HashBlock, int)> callback) {
